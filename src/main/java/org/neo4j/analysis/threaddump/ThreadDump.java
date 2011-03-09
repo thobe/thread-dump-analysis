23e721e8cb906353261e82b4bcd08b45582b6759
Tobias Ivarsson <tobias@thobe.org>
2011-03-09 17:42:25 +0100

    Updated to most recent Neo4j parent pom.

--- src/main/java/org/neo4j/analysis/threaddump/ThreadDump.java
+++ src/main/java/org/neo4j/analysis/threaddump/ThreadDump.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright (c) 2002-2011 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.neo4j.analysis.threaddump;
 
 import java.io.File;
@@ -19,8 +38,8 @@ public class ThreadDump implements Iterable<ThreadData>
 
     ThreadDump( String[] header, Collection<ThreadData> threads )
     {
-        this.date = header[0];
-        this.info = header[1];
+        this.date = header == null ? "unkown" : header[0];
+        this.info = header == null ? "unkown" : header[1];
         this.threads = threads;
         this.monitors = new HashMap<String, MonitorState>();
         for ( ThreadData thread : threads )
